1ce7b15d0275556e4261d20da0e52a2dcd3b8896
Ignacio Lopez Vellon <developerobarrio@gmail.com>
2017-07-16 17:29:21 +0200

    Se añade el controlador de la base de datos

--- src/main/java/org/openshift/InsultGenerator.java
+++ src/main/java/org/openshift/InsultGenerator.java
@@ -4,8 +4,10 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.logging.Logger;
 
 public class InsultGenerator {
+    Logger LOGGER = Logger.getLogger("InsultGenerator");
     public String generateInsult() {
         String vowels = "AEIOU";
         String article = "an";
@@ -14,15 +16,22 @@ public class InsultGenerator {
         try {
             String databaseURL = "jdbc:postgresql://";
             databaseURL += System.getenv("POSTGRESQL_SERVICE_HOST");
+            LOGGER.info(databaseURL);
             databaseURL += "/" + System.getenv("POSTGRESQL_DATABASE");
+            LOGGER.info(databaseURL);
             String username = System.getenv("POSTGRESQL_USER");
+            LOGGER.info(databaseURL);
             String password = System.getenv("PGPASSWORD");
+            LOGGER.info(databaseURL);
             Connection connection = DriverManager.getConnection(databaseURL, username, password);
+            LOGGER.info(databaseURL);
             if (connection != null) {
                 String SQL = "select a.string AS first, b.string AS second, c.string AS noun" +
                         "from short_adjective a , long_adjective b, noun c ORDER BY random() limit 1";
                 Statement stmt = connection.createStatement();
+            LOGGER.info(databaseURL);
                 ResultSet rs = stmt.executeQuery(SQL);
+            LOGGER.info(databaseURL);
                 while (rs.next()) {
                     if (vowels.indexOf(rs.getString("first").charAt(0)) == -1) {
                         article = "a";
@@ -35,7 +44,7 @@ public class InsultGenerator {
             }
         } catch (Exception e) {
 
-            System.out.println(e.getCause());
+            e.printStackTrace();
             return "Database connection problem!" + e.getMessage();
 
         }
