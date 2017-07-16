4b90cace87274bff14585eed303ba2367acb8402
Ignacio Lopez Vellon <developerobarrio@gmail.com>
2017-07-16 18:12:01 +0200

    Se añade el controlador de la base de datos

---
+++ src/main/java/org/openshift/JaxrsConfig.java
@@ -0,0 +1,11 @@
+package org.openshift;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Created by nacho on 16/07/17.
+ */
+@ApplicationPath("/api")
+public class JaxrsConfig extends Application{
+}
